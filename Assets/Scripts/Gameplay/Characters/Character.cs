9c6b4f5fcbf7501356a132dc623f3cdb3adfb976
Ensuque <khamaz.ee@gmail.com>
2019-11-09 12:06:57 -0500

    Basic movement controls

---
+++ Assets/Scripts/Gameplay/Characters/Character.cs
@@ -0,0 +1,79 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace uqac.gameplay
+{
+    public class Character : MonoBehaviour
+    {
+
+        [ShowInInspector]
+        protected float speed = 3f;
+
+        private Rigidbody2D rb;
+
+        //Trigger on position change, (PreviousPos, NewPos)
+        protected Action<Vector2, Vector2> OnPositionChange = null;
+
+        //region Properties
+
+        public Vector2 Position
+        {
+            get => transform.position;
+            set
+            {
+                OnPositionChange?.Invoke(rb.position, value);
+                rb.MovePosition(value);
+            }
+        }
+
+        //endregion
+
+
+        //region MonoBehaviour Loop
+
+        protected void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+
+            OnPositionChange += TryRotateTowardNewPosition; //lambda function
+
+            rb.velocity = Vector2.right;
+        }
+
+        //endregion
+
+
+        protected void MoveToward(Vector2 worldPos)
+        {
+
+            Vector2 direction = (worldPos - rb.position).normalized;
+
+            Vector2 deltaMovement = speed * Time.deltaTime * direction;
+
+            //rb.velocity = deltaMovement;
+            Position += deltaMovement;
+        }
+
+        protected void TryRotateTowardNewPosition(Vector2 oldP, Vector2 newP)
+        {
+            Vector2 dir = newP - oldP;
+            RotateToward(newP);
+        }
+
+        protected void RotateToward(Vector2 worldPos)
+        {
+
+            Vector2 remainingDistance = (worldPos - (Vector2)transform.position);
+            Vector2 direction = remainingDistance.normalized;
+
+            float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = q;
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, q, RotateSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Characters/MainCharacter.cs
@@ -0,0 +1,48 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace uqac.gameplay
+{
+    public class MainCharacter : Character
+    {
+
+
+        //endregion
+
+
+        //region MonoBehaviour Loop
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleMovements();
+
+            
+
+        }
+
+        //endregion
+
+        private void HandleMovements()
+        {
+
+            //Read the direction of the current inputs
+            Vector2 inputDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            if (inputDirection.magnitude < Mathf.Epsilon)
+            {
+                return;
+            }
+
+            //They are normalized for constant speed in all directions.
+            inputDirection = inputDirection.normalized;
+
+            MoveToward(Position + inputDirection);
+
+        }
+
+    }
+}
