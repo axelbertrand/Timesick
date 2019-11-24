ec9a7d26c95a9f3cbfe8b56ee44f79fcd8016836
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-11-23 20:26:32 -0500

    add visual feedback when exit appears

--- Assets/Scripts/Gameplay/LevelExit.cs
+++ Assets/Scripts/Gameplay/LevelExit.cs
@@ -9,8 +9,21 @@ namespace uqac.timesick.gameplay
         [SerializeField]
         private MainCharacter mainCharacter;
 
+        [SerializeField]
+        private float feedbackCircleRange = 15;
+
+        [SerializeField]
+        private float feedbackCircleSpeed = 15;
+
+        [SerializeField]
+        private float feedbackCircleThickness = 2;
+
         private SpriteRenderer spriteRenderer;
         private Collider2D circleCollider;
+        private Transform feedbackTransform;
+        private Transform feedbackMaskTransform;
+
+        private float timeSincefeedbackActivation = 0;
 
         private bool isEnabled;
         public bool IsEnabled
@@ -24,25 +37,52 @@ namespace uqac.timesick.gameplay
             }
         }
 
+        private bool isFeedbackActive;
+
         // Start is called before the first frame update
         void Start()
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
             circleCollider = GetComponent<CircleCollider2D>();
+            feedbackTransform = GetComponentsInChildren<Transform>()[1];
+            feedbackMaskTransform = GetComponentsInChildren<Transform>()[2];
             IsEnabled = false;
             mainCharacter.MedicineStolenEvent += OnMedicineStolenEvents;
+        }
+
+        private void Update()
+        {
+            if(isFeedbackActive)
+            {
+                UpdateFeedback();
+            }
         }
 
         void OnMedicineStolenEvents()
         {
             IsEnabled = true;
-            // Add sound and visual effects
+            isFeedbackActive = true;
+
+            // Add sound effects
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             Debug.Log("End of level");
             // Animations + go to debrief scene
+        }
+
+        private void UpdateFeedback()
+        {
+            timeSincefeedbackActivation += Time.deltaTime;
+            float feedbackCircleSize = feedbackCircleSpeed * timeSincefeedbackActivation;
+            feedbackTransform.localScale = new Vector3(feedbackCircleSize, feedbackCircleSize, 1);
+            feedbackMaskTransform.localScale = new Vector3(feedbackCircleSize - feedbackCircleThickness, feedbackCircleSize - feedbackCircleThickness, 1);
+            if (feedbackCircleSize > feedbackCircleRange)
+            {
+                feedbackTransform.localScale = new Vector3();
+                isFeedbackActive = false;
+            }
         }
     }
 }
