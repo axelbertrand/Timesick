beb2d7db15dcfed49f2a18d446f08e28347920c2
Fab <fab.mckay@gmail.com>
2019-11-27 14:35:32 -0500

    :sparkles: Briefing

---
+++ Assets/Scripts/Story/Briefing.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Briefing", menuName = "ScriptableObjects/Dialogues/Briefing")]
+public class Briefing : ScriptableObject
+{
+    public Sprite image;
+
+    [TextArea]
+    public string text;
+}
---
+++ Assets/Scripts/Story/BriefingManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BriefingManager : MonoBehaviour
+{
+    public Image image;
+    public Briefing briefing;
+    public TextMeshProUGUI textBox;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.sprite = briefing.image;
+        textBox.text = briefing.text;
+    }
+
+    public void StartLevel()
+    {
+        GameManager.Instance.LoadMainLevel();
+    }
+   
+}
